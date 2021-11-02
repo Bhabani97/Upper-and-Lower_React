9979c680afd7a7fdba8c9f8166f95c606297518b
bhabani97 <bhabanisankardash61@gmail.com>
2021-11-02 14:27:00 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" />
+      <div className="container" my-3>
+        <TextForm  heading="Enter your Text to Analyze Below" />
+      </div>
+
+
+
+
+
+    </>
+  );
+}
+
+export default App;
